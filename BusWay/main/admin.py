12e9c3f8de3708c4105a7d4a6f42ecf1b2d89b7b
Amir <70170580+murtanli@users.noreply.github.com>
2024-05-10 02:54:55 +0300

    Ver 0.5

---
+++ BusWay/main/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'user_login')
+    list_display_links = ('user_login',)
+
+    def user_login(self, obj):
+        return obj.user_id.username
+
+    user_login.short_description = 'User Login'
+
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'brand', 'total_seats', 'license_plate')
+
+@admin.register(BusSeat)
+class BusSeatAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'seat_number', 'is_occuiped', 'bus_license_plate')
+    list_display_links = ('bus_license_plate',)
+
+    def bus_license_plate(self, obj):
+        return obj.bus.license_plate
+
+    bus_license_plate.short_description = 'License plate'
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'origin', 'destination', 'departure_time', 'arrival_time', 'duration', 'distance')
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'route_name', 'license_plate_bus', 'available_seats', 'price')
+    list_display_links = ('route_name', 'license_plate_bus',)
+
+    def route_name(self, obj):
+        return f"{obj.route.origin} - {obj.route.destination}"
+
+    route_name.short_description = "Route name"
+
+    def license_plate_bus(self, obj):
+        return obj.bus.license_plate
+
+    license_plate_bus.short_description = "License plate"
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user_full_name', 'schedule_route', 'selected_seat', 'ticket_status')
+    list_display_links = ('user_full_name', 'schedule_route', 'selected_seat')
+
+    def user_full_name(self, obj):
+        return f"{obj.profile.first_name} {obj.profile.last_name}"
+
+    user_full_name.short_description = "User full name"
+
+    def schedule_route(self, obj):
+        route = obj.schedule.route
+        return f"{route.origin} - {route.destination}"
+
+    schedule_route.short_description = "Schedule route"
+
+    def selected_seat(self, obj):
+        return f"{obj.sel_seat.bus.license_plate} - {obj.sel_seat.seat_number}"
+
+    selected_seat.short_description = "Selected seat"
---
+++ BusWay/main/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+
+class Bus(models.Model):
+    brand = models.CharField(max_length=100)
+    total_seats = models.IntegerField()
+    license_plate = models.CharField(max_length=9)
+
+class BusSeat(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    seat_number = models.IntegerField()
+    is_occuiped = models.BooleanField(default=False)
+
+class Route(models.Model):
+    origin = models.CharField(max_length=30)
+    destination = models.CharField(max_length=30)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    duration = models.IntegerField()
+    distance = models.IntegerField()
+
+class Schedule(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    available_seats = models.IntegerField()
+    price = models.IntegerField()
+
+class Ticket(models.Model):
+    status = (
+        ('Забронировано', 'Забронировано'),
+        ('Ждет оплаты', 'Ждет оплаты'),
+        ('Оплачен', 'Оплачен')
+    )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    sel_seat = models.ForeignKey(BusSeat, on_delete=models.CASCADE)
+    ticket_status = models.CharField(choices=status, max_length=30)
